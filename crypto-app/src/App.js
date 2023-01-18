e35c0f628afa5f8fee6c283c48604a8e56a078f5
Janne Peiponen <janne.peiponen@edu.karelia.fi>
2023-01-18 18:55:33 +0200

    pages

--- crypto-app/src/App.js
+++ crypto-app/src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Homepage from "./pages/Homepage";
+import Coin from "./pages/Coin";
+
+//https://www.youtube.com/watch?v=QA6oTpMZp84
+
+//https://www.youtube.com/watch?v=fzxEECHnsvU
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/coins/:id" element={<Coin />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
